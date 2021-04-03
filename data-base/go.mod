beaddbad80e541c687a022641bdbe665620801ba
squeezesky <squeezesky@antonosintsev2000@gmail.com>
2021-04-03 11:35:57 +0500

    new files

--- data-base/go.mod
+++ data-base/go.mod
@@ -1,3 +1,5 @@
 module main
 
 go 1.16
+
+require github.com/squeezesky/restless-program v0.0.0-20210403062020-85e7d4212479
--- data-base/main.go
+++ data-base/main.go
@@ -1,10 +1,20 @@
 package main 
 
 import (
-  "github.com/squeezesky/restless-program/handlers"
+  "github.com/squeezesky/restless-program/data-base/handlers"
   "log"
+  "net/http"
+  "os"
 )
 
 func main(){
   l := log.New(os.Stdout,"product-api",log.LstdFlags)
+
+  hh := handlers.NewHello(l)
+  
+  sm := http.NewServeMux()
+  sm.Handle("/",hh)
+
+  http.ListenAndServe(":8080",nil)
+
 }
